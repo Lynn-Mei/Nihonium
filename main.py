18fa1092cb29738d8582ba91264ff8e9fa0bc01e
Hugo-Mrula <hugo.murura@gmail.com>
2024-07-28 16:56:02 +0200

    Refactor du main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
-from weasyprint import HTML
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
 
-class MyWidget(QtWidgets.QWidget):
+from CardManager import CardManager
+
+class CardListing(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
@@ -23,11 +27,19 @@ class MyWidget(QtWidgets.QWidget):
     def magic(self):
         self.text.setText(random.choice(self.hello))
         
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.start()
+        
+    def start(self):
+        widget = CardListing()
+        self.setCentralWidget(widget)
+        self.resize(800, 600)
+    
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
-    widget = MyWidget()
-    widget.resize(800, 600)
+    app = QApplication([])
+    widget = MainWindow()
     widget.show()
-
     sys.exit(app.exec())
+    
