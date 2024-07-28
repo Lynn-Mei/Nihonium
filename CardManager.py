0fc7d8e96f17e8070b3424a466fcc4f4ebc475cb
Hugo-Mrula <hugo.murura@gmail.com>
2024-07-28 16:39:20 +0200

    Creating Manager class with base methods

---
+++ CardManager.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+class CardManager:
+    def __init__(self):
+        self.appdata_path = os.path.join(os.environ['APPDATA'], "Nihonium")
+        os.makedirs(self.appdata_path, exist_ok=True)
+        
+    def createCardGroup(self, name:str = None) -> str:
+        if name == None:
+            name = "cardsGroup"+str(random.randint(0, 10000))+".json"
+        file_path = os.path.join(self.appdata_path, name)
+        with open(file_path, "w") as f:
+            f.write(name)
+        print(f"File created:" +str(file_path))
+        return file_path
+    
+    def importCardGroup(self, name:str = "cards.json"):
+        file_path = os.path.join(self.appdata_path, name)
+        with open(file_path, "r") as f:
+            for line in f:
+                print(line.strip())
