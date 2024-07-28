c47fdadde75be1e74dc379d0685e74429129460a
Hugo-Mrula <hugo.murura@gmail.com>
2024-07-28 16:58:17 +0200

    Moving away CardListing

--- main.py
+++ main.py
@@ -5,28 +5,10 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
 
+from CardListing import CardListing
 from CardManager import CardManager
 
-class CardListing(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
-        
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
