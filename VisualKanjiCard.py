76c276e4a234f5c65560ca488cfeea917cbcfde3
Hugo-Mrula <hugo.murura@gmail.com>
2024-08-04 14:59:45 +0200

    Upgrade of the visual kanji card

--- VisualKanjiCard.py
+++ VisualKanjiCard.py
@@ -5,10 +5,20 @@ class VisualKanjiCard(QtWidgets.QWidget):
     def __init__(self, card: Kanjicard):
         super().__init__()
         self.card = card
-        self.kanji = QtWidgets.QLabel(card.kanji, alignment=QtCore.Qt.AlignLeft)
-        self.kana = QtWidgets.QLabel(card.readings[0], alignment=QtCore.Qt.AlignCenter)
-        self.meaning = QtWidgets.QLabel(card.meanings[0] , alignment=QtCore.Qt.AlignRight)
-        self.layout = QtWidgets.QVBoxLayout(self)
+        
+        self.setFixedSize(200, 75)
+        self.kanji = QtWidgets.QLabel(card.kanji)
+        self.kana = QtWidgets.QLabel(card.readings[0])
+        self.meaning = QtWidgets.QLabel(card.meanings[0])
+        
+        self.kanji.setStyleSheet("font-size: 21px;")
+        self.kana.setStyleSheet("font-size: 21px;")
+        self.meaning.setStyleSheet("font-size: 21px;")
+        
+        self.layout = QtWidgets.QHBoxLayout(self)
         self.layout.addWidget(self.kanji)
         self.layout.addWidget(self.kana)
         self.layout.addWidget(self.meaning)
+        
+        self.setStyleSheet("background-color: lightblue;")
+        
