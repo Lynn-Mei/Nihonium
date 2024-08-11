255f861f2a82cecc46f5a57a3a31433230c3b1f7
Hugo-Mrula <hugo.murura@gmail.com>
2024-08-11 18:39:14 +0200

    Coding pages function to open a kanji group

--- Book/Pages.py
+++ Book/Pages.py
@@ -5,24 +5,38 @@ class Pages:
         self.KanjiListing = []
         self.VocListing = []
         '''Les KanjiCardGroup seront gardes ici, il faudra eventuellement optimiser pour eviter d'en garder trop '''
-        self.kanjigroups = []
-        self.vocgroups = []
+        self.kanjigroups = {}
+        self.vocgroups = {}
         parser = XmlLoader(lines)
         tempCollection = parser.parseMain()
         self.KanjiListing = tempCollection[0]
         self.VocListing = tempCollection[1]
         
-    def openKanjiGroup(index: int)->KanjiCardGroup:
+    def openKanjiGroup(self, index: int)->KanjiCardGroup:
         group: KanjiCardGroup = None
-        '''si fichier contenu dans les keep alors revoyer direct'''
-        '''ouverture du fichier correspondant a l'index '''
-        '''Parsing et creation d'un KanjiCardGroup'''
-        '''Ajout a la liste'''
+        file = self.KanjiListing[index]
+        if file in self.kanjigroups.keys():
+            group = self.kanjigroups[file]
+        else:
+            fileLines = self.getGroupFile(file)
+            parser = XmlLoader(fileLines)
+            group = parser.parseKanjiGroup()
+        self.kanjigroups[file] = group
         return group
         
     '''def openVocGroupPage()->VocCardGroup:
         return VocCardGroup()'''
         
+    ''' ToDO : Definir le chemin Nihonium en constante '''
+    def getGroupFile(self, file:str) -> list:
+        path = os.path.join(os.environ['APPDATA'], "Nihonium")
+        path = os.path.join(path, file)
+        lines = []
+        with open(path, "r") as f:
+            for line in f:
+                lines.append(line.strip())
+        return lines
+    
     def toXml()->str:
         return ["to", "code"]
         
