53922b0e932ba92f81166a1f36b7c05f92b21275
Hugo-Mrula <hugo.murura@gmail.com>
2024-09-01 15:43:43 +0200

    Coding listing click in pages

--- Book/VisualPages.py
+++ Book/VisualPages.py
@@ -1,5 +1,6 @@
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtWidgets import QGridLayout, QVBoxLayout, QLabel, QListWidget, QListWidgetItem, QScrollArea
+from Cards.Kanji.VisualKanjiCardGroup import VisualKanjiCardGroup
 
 class VisualPages(QtWidgets.QWidget):
     def __init__(self, pages):
@@ -9,12 +10,13 @@ class VisualPages(QtWidgets.QWidget):
         self.left_nav_layout = QtWidgets.QVBoxLayout()
         
         self.book_title = QLabel(self.pages.getBookTitle())
+        self.book_title.setMaximumWidth(400)
         self.left_nav_layout.addWidget(self.book_title)
         self.layout.addLayout(self.left_nav_layout, 0, 0)
         self.generate_grouplist()
 
         self.main = QLabel("")
-        self.main.setMinimumWidth(700)
+        '''self.main.setMinimumWidth(700)'''
         self.layout.addWidget(self.main, 0, 1)
         '''creer nouvelle classe VisualPageGroup pour le code suivant'''
         '''cette classe doit avoir un id donne en init et override mousePressEvent et utiliser une fonction callback'''
@@ -22,15 +24,29 @@ class VisualPages(QtWidgets.QWidget):
         
     def generate_grouplist(self):
         self.list_widget = QListWidget()
+        count = 0
+        groups_paths = self.pages.getKanjiGroupsPaths()
         for i in self.pages.getKanjiGroups():
             item = QListWidgetItem(i)
+            item.setData(1, count)
             self.list_widget.addItem(item)
+            count+=1
+            
+        self.list_widget.itemClicked.connect(self.page_clicked_kan)
         scroll_area = QScrollArea()
         scroll_area.setWidgetResizable(True)
         scroll_area.setWidget(self.list_widget)
+        scroll_area.setMaximumWidth(400)
         self.left_nav_layout.addWidget(scroll_area)
         
-    def page_clicked(self, type, index):
+    def page_clicked_kan(self, item):
+        return self.page_clicked(item, "Kanji")
+        
+    def page_clicked_voc(self, item):
+        return self.page_clicked(item, "Voc")
+        
+    def page_clicked(self, item, type):
+        index = item.data(1)
         group = None
         self.layout.removeWidget(self.main)
         if type == "Kanji":
@@ -38,6 +54,6 @@ class VisualPages(QtWidgets.QWidget):
         else:
             group = self.pages.openVocGroup(index)
         self.main = VisualKanjiCardGroup(group)
-        self.layout.addWidget(self.main)
+        self.layout.addWidget(self.main, 0, 1)
         '''Retirer tout visualkanji card group precedent'''
         
