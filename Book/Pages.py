234a9faa68405b3cdb91a6ba90483b9f07a1cbf8
Hugo-Mrula <hugo.murura@gmail.com>
2024-09-01 15:05:45 +0200

    First implement of visual pages

--- Book/Pages.py
+++ Book/Pages.py
@@ -3,16 +3,24 @@ from .XmlLoader import XmlLoader
 from Cards.Kanji.KanjiCardGroup import KanjiCardGroup
 
 class Pages:
-    def __init__(self, lines:list):
+    def __init__(self, lines:list, path: str):
         self.KanjiListing = []
         self.VocListing = []
         '''Les KanjiCardGroup seront gardes ici, il faudra eventuellement optimiser pour eviter d'en garder trop '''
         self.kanjigroups = {}
         self.vocgroups = {}
-        parser = XmlLoader(lines, "")
+        parser = XmlLoader(lines, path)
         tempCollection = parser.parseMain()
         self.KanjiListing = tempCollection[0]
         self.VocListing = tempCollection[1]
+        self.title = tempCollection[2]
+        self.KanTitles = tempCollection[3]
+        
+    def getBookTitle(self):
+        return self.title
+        
+    def getKanjiGroups(self):
+        return self.KanTitles
         
     def openKanjiGroup(self, index: int)->KanjiCardGroup:
         group: KanjiCardGroup = None
--- Book/VisualPages.py
+++ Book/VisualPages.py
@@ -1,20 +1,43 @@
 from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import QGridLayout, QVBoxLayout, QLabel, QListWidget, QListWidgetItem, QScrollArea
 
 class VisualPages(QtWidgets.QWidget):
     def __init__(self, pages):
         super().__init__()
         self.pages = pages
-        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout = QtWidgets.QGridLayout(self)
+        self.left_nav_layout = QtWidgets.QVBoxLayout()
+        
+        self.book_title = QLabel(self.pages.getBookTitle())
+        self.left_nav_layout.addWidget(self.book_title)
+        self.layout.addLayout(self.left_nav_layout, 0, 0)
+        self.generate_grouplist()
+
+        self.main = QLabel("")
+        self.main.setMinimumWidth(700)
+        self.layout.addWidget(self.main, 0, 1)
         '''creer nouvelle classe VisualPageGroup pour le code suivant'''
         '''cette classe doit avoir un id donne en init et override mousePressEvent et utiliser une fonction callback'''
         '''ajouter la section gauche qui liste les card groups (onglet voc/kanji)'''
         
+    def generate_grouplist(self):
+        self.list_widget = QListWidget()
+        for i in self.pages.getKanjiGroups():
+            item = QListWidgetItem(i)
+            self.list_widget.addItem(item)
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setWidget(self.list_widget)
+        self.left_nav_layout.addWidget(scroll_area)
+        
     def page_clicked(self, type, index):
         group = None
+        self.layout.removeWidget(self.main)
         if type == "Kanji":
             group = self.pages.openKanjiGroup(index)
         else:
             group = self.pages.openVocGroup(index)
-        self.layout.addWidget(VisualKanjiCardGroup(group))
+        self.main = VisualKanjiCardGroup(group)
+        self.layout.addWidget(self.main)
         '''Retirer tout visualkanji card group precedent'''
         
