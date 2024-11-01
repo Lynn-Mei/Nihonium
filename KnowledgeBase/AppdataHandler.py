66aa2db7ecb773a8e0d01a858c6a0d2a7c0af460
Hugo-Mrula <hugo.murura@gmail.com>
2024-11-01 15:48:58 +0100

    Adding a request executor to appdata database

--- KnowledgeBase/AppdataHandler.py
+++ KnowledgeBase/AppdataHandler.py
@@ -13,8 +13,12 @@ class AppdataHandler():
         kanjis += "'" + list(data)[0] + "')"
         
         query = '''SELECT * FROM Kanji k LEFT JOIN KanjiReadings r ON r.Kanji = k.Kanji WHERE k.Kanji IN '''+ kanjis +''' OR r.Reading LIKE "%'''+ data +'''%" GROUP BY k.Kanji'''
+        return self.execute(query)
+
+    def execute(self, request: str) -> list:
+        rows = []
         with sqlite3.connect(self.db_path) as conn:
             cursor = conn.cursor()
-            cursor.execute(query)
+            cursor.execute(request)
             rows = cursor.fetchall()
         return rows
